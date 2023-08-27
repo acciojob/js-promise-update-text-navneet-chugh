3d9d964e03673e217507e6749b27461a83276e88
navneet-chugh <navneetchugh15@gmail.com>
2023-08-27 21:43:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateTextWithPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000); // 1 second delay
+  });
+}
+
+const outputElement = document.getElementById('output');
+
+updateTextWithPromise().then((message) => {
+  outputElement.textContent = message;
+});
